0f236644bb7d73ccb247f146b0cfc2c6b46df8a8
admin <penghua.wang@condenast.com.cn>
2019-01-25 15:32:44 +0800

    __strong

--- InterView/InterView/Block/BlockViewController.m
+++ InterView/InterView/Block/BlockViewController.m
@@ -22,6 +22,45 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
     
+    [self test2];
+    
+}
+
+- (void)test3{
+    
+}
+
+- (void)test2{
+    MyObject *myObject = [[MyObject alloc]init];
+    HZLog(@"obj", myObject); //变量内存地址：0x7ffee7686448, 变量值：0x6000000109a0, 指向对象值：<MyObject: 0x6000000109a0>, --> obj
+    __weak MyObject *weakObj = myObject;
+    HZLog(@"weakObj-1", weakObj); //变量内存地址：0x7ffee7686440, 变量值：0x6000000109a0, 指向对象值：<MyObject: 0x6000000109a0>, --> weakObj-1
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        __strong MyObject *strongObj = weakObj;
+        HZLog(@"weakObj-block", weakObj); //变量内存地址：0x600000c4e3c0, 变量值：0x6000000109a0, 指向对象值：<MyObject: 0x6000000109a0>, --> weakObj-block
+        HZLog(@"strongObj-block", strongObj); //0x7000058eed48, 变量值：0x6000000109a0, 指向对象值：<MyObject: 0x6000000109a0>, --> strongObj-block
+        
+        sleep(3);
+        NSLog(@"sleep 3s -----------------");
+        if (strongObj) {
+            
+            HZLog(@"weakObj-block", weakObj); //变量内存地址：0x600000c4e3c0, 变量值：0x6000000109a0, 指向对象值：<MyObject: 0x6000000109a0>, --> weakObj-block
+            HZLog(@"strongObj-block", strongObj); //变量内存地址：0x7000058eed48, 变量值：0x6000000109a0, 指向对象值：<MyObject: 0x6000000109a0>, --> strongObj-block
+        }
+    });
+    sleep(1);
+    NSLog(@"sleep 1s -----------------");
+    myObject = nil;
+    sleep(5);
+    NSLog(@"sleep 5s -----------------");
+    HZLog(@"weakObj-2", weakObj); //变量内存地址：0x7ffee7686440, 变量值：0x0, 指向对象值：(null), --> weakObj-2
+    /*
+     从上述结果可以看出:
+        只要block内部的代码开始执行,block内部就会通过__strong在执行期间进行强引用操作,保证block内部strongObj始终是可用的.因此我们可以在使用strongObj的时候使用
+     if(strongObj){}进行使用,先判断strongObj是否已经被释放再执行后续代码
+     */
+}
+- (void)test1{
     /*
      __weak
      */
